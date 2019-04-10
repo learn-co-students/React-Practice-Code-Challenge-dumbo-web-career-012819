e457df6a79c9858f2c16d9e664166bdc522da878
MichaelRomero <liikemike@aim.com>
2019-04-09 22:43:17 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,51 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushiList: [],
+    sushiEaten: [],
+    money: 100,
+    index: {
+      end: 4,
+      start: 0
+    }
+  }
+
+  handleIndexClick = () => {
+    this.setState({
+      index: {
+        end: this.state.index.end + 4,
+        start: this.state.index.start + 4
+      }
+    })
+  }
+
+  handleSushiClick = (sushi) => {
+    if (this.state.money - sushi.price > -1) {
+      this.setState(state => ({
+        sushiEaten: [...state.sushiEaten, sushi],
+        money: state.money - sushi.price
+      }))
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(sushiJSON => this.setState({sushiList: sushiJSON}))
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushiList={this.state.sushiList}
+          index={this.state.index}
+          money={this.state.money}
+          handleIndexClick={this.handleIndexClick}
+          handleSushiClick={this.handleSushiClick}
+          />
+        <Table money={this.state.money} sushiEaten={this.state.sushiEaten}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from 'react'
+import Sushi from '../components/Sushi'
 import MoreButton from '../components/MoreButton'
 
 const SushiContainer = (props) => {
+
+  // const filterSushi = props.sushiList.filter((sushi, i) => i < props.index)
+  const firstFour = props.sushiList.slice(props.index.start, props.index.end)
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          firstFour.map(sushi => <Sushi key={sushi.id} sushi={sushi} money={props.money} handleSushiClick={props.handleSushiClick}/>)
         }
-        <MoreButton />
+        <MoreButton handleIndexClick={props.handleIndexClick}/>
       </div>
     </Fragment>
   )
