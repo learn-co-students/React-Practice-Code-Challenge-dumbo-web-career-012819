bda6d7f3c236e42e2ac4fa187d21ce1837480142
Angelo Spampinato <spampinatodesign@gmail.com>
2019-04-10 10:09:30 -0400

    done

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,42 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+class Sushi extends Component {
+
+  state = {
+    eaten: false
+  }
+
+  handleEaten = () => {
+    if(this.props.customerMoney - this.props.sushi.price >= 0 ) {
+      this.setState({
+        eaten: true
+      }, () => this.props.eatenSushiArray(this.state.eaten))
+
+      this.props.reduceMoney(this.props.sushi.price)
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.sushi.id !== nextProps.sushi.id) {
+      this.setState({
+        eaten: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="sushi">
+        <div className="plate" 
+             onClick={this.handleEaten}>
+          { this.state.eaten ? null : <img src={this.props.sushi.img_url} width="100%" /> }
+        </div>
+        <h4 className="sushi-details">
+          {this.props.sushi.name} - ${this.props.sushi.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    )
+  }
 }
 
 export default Sushi
